cb246ef01986826da8373598a7bcd2fbf55cfd85
Kayo Phoenix <kayo@illumium.org>
2018-04-22 23:10:07 +0500

    Reorganized code

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -7,3 +7,10 @@ pub use self::mailbox::*;
 pub use self::textual::*;
 pub use self::special::*;
 pub use self::content::*;
+
+pub use hyper::header::{
+    Headers, Header,
+    ContentType, ContentLocation, ContentDisposition,
+    DispositionType, DispositionParam,
+    Date, HttpDate as EmailDate
+};
